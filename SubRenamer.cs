c809638efd675714e2829b31b66f4278747b0832
SubZeroPL <subzeropl@gmail.com>
2024-08-13 11:19:48 +0200

    updated to shoko abstraction 4.0.0-beta1

--- SubRenamer.cs
+++ SubRenamer.cs
@@ -6,85 +6,94 @@ using Microsoft.Extensions.Logging;
 using Shoko.Plugin.Abstractions;
 using Shoko.Plugin.Abstractions.Attributes;
 using Shoko.Plugin.Abstractions.DataModels;
+using Shoko.Plugin.Abstractions.Events;
 
 namespace SubRenamer
 {
-    [Renamer(nameof(SubRenamer))]
+    [RenamerID("SubRenamer")]
+    // ReSharper disable once UnusedType.Global
     public class SubRenamer : IRenamer
     {
         private readonly ILogger<SubRenamer> _logger;
+
+        public string Name => "SubRenamer";
+        public string Description => "SubRenamer";
+        public bool SupportsMoving => true;
+        public bool SupportsRenaming => true;
 
         public SubRenamer(ILogger<SubRenamer> logger)
         {
             _logger = logger;
         }
 
-        public string GetFilename(MoveEventArgs args)
+        private (RelocationError, string) GetFilename(RelocationEventArgs args)
         {
             _logger.LogInformation("GetFilename");
 
             var finalName = "";
 
+            var extension = Path.GetExtension(args.File.FileName);
+            if (args.Series.Count == 0)
+            {
+                args.Cancel = true;
+                return (new RelocationError("Anime not identified"), null);
+            }
+
+            var anime = args.Series[0];
+
+            var type = anime.Type;
+            if (args.Episodes.Count == 0)
+            {
+                args.Cancel = true;
+                return (new RelocationError("Episode not identified"), null);
+            }
+
+            var episode = args.Episodes[0];
+
+            var isSpecial = episode.Type != EpisodeType.Episode;
+
+            if (anime.EpisodeCounts.Episodes == 1 && !isSpecial)
+            {
+                var title = episode.Titles.FirstOrDefault(t => t.Language == TitleLanguage.English)?.Title ??
+                            episode.Titles.FirstOrDefault(t => t.Language == TitleLanguage.Romaji)?.Title ?? "";
+                if (Regex.IsMatch(title, "Episode \\d+"))
+                    title = type == AnimeType.Movie ? "Complete Movie" : anime.PreferredTitle;
+
+                finalName += title;
+            }
+            else
+            {
+                var prefix = "";
+                if (isSpecial) prefix = episode.Type.ToString()[..1];
+
+                finalName += $"{prefix}{episode.EpisodeNumber.PadZeroes(anime.EpisodeCounts.Episodes)} - ";
+                var title = episode.Titles.FirstOrDefault(t => t.Language == TitleLanguage.English)?.Title ??
+                            episode.Titles.FirstOrDefault(t => t.Language == TitleLanguage.Romaji)?.Title ?? "";
+
+                finalName += title;
+            }
+
+            var groupName = "";
+
             try
             {
-                var extension = Path.GetExtension(args.File.FileName);
-                if (args.Series.Count == 0) throw new Exception("Anime not identified");
-                var anime = args.Series[0];
-
-                var type = anime.Type;
-                if (args.Episodes.Count == 0) throw new Exception("Episode not identified");
-                var episode = args.Episodes[0];
-                var episodeCount = episode.Series?.EpisodeCounts.Episodes ?? 0; //anime.EpisodeCounts.Episodes;
-
-                var isSpecial = episode.Type != EpisodeType.Episode;
-                
-                if (episodeCount == 1 && !isSpecial)
-                {
-                    var title = episode.Titles.FirstOrDefault(t => t.Language == TitleLanguage.English)?.Title ??
-                                episode.Titles.FirstOrDefault(t => t.Language == TitleLanguage.Romaji)?.Title ?? "";
-                    if (Regex.IsMatch(title, "Episode \\d+"))
-                        title = type == AnimeType.Movie ? "Complete Movie" : anime.PreferredTitle;
-
-                    finalName += title;
-                }
-                else
-                {
-                    var prefix = "";
-                    if (isSpecial) prefix = episode.Type.ToString()[..1];
-
-                    finalName += $"{prefix}{episode.EpisodeNumber.PadZeroes(episodeCount)} - ";
-                    var title = episode.Titles.FirstOrDefault(t => t.Language == TitleLanguage.English)?.Title ??
-                                episode.Titles.FirstOrDefault(t => t.Language == TitleLanguage.Romaji)?.Title ?? "";
-
-                    finalName += title;
-                }
-
-                var groupName = "";
-                
-                try
-                {
-                    groupName = args.Video.AniDB?.ReleaseGroup.ShortName;
-                    groupName = groupName != null ? $" [{groupName}]" : "";
-                }
-                catch
-                {
-                    _logger.LogInformation("Release group unknown");
-                }
-
-                finalName += groupName;
-                finalName = (finalName + extension).RemoveInvalidPathCharacters();
-                _logger.LogInformation("FinalName = {finalName}", finalName);
+                groupName = args.Episodes[0].AnidbEpisode.VideoList[0].AniDB?.ReleaseGroup.ShortName;
+                groupName = groupName != null ? $" [{groupName}]" : "";
             }
-            catch (Exception e)
+            catch
             {
-                _logger.LogError(e, "GetFilename error");
-                throw;
+                _logger.LogInformation("Release group unknown");
             }
 
-            return finalName;
+            finalName += groupName;
+
+            finalName = (finalName + extension).RemoveInvalidPathCharacters();
+            _logger.LogInformation("FinalName = {finalName}", finalName);
+
+            return (null, finalName);
         }
 
-        public (IImportFolder destination, string subfolder) GetDestination(MoveEventArgs args)
+        private (RelocationError, string) GetDestination(RelocationEventArgs args)
         {
             _logger.LogInformation("GetDestination");
 
@@ -92,44 +101,61 @@ namespace SubRenamer
              * dropFolder\type\romajiName [year]
              */
 
-            IImportFolder dropFolder = null;
-            var finalDest = "";
+            if (args.Series.Count == 0)
+            {
+                args.Cancel = true;
+                return (new RelocationError("Anime not identified"), null);
+            }
+
+            // first determine anime type
+            var anime = args.Series[0];
+            // we use type as string
+            var type = anime.Type.ToString();
+
+            // now we determine anime name to use
+            var romajiName = anime.PreferredTitle.RemoveInvalidPathCharacters();
+
+            // and finally year
+            var year = anime.AirDate.HasValue ? anime.AirDate.Value.Year.ToString() : "";
+
+            var finalDest = Path.Combine(type,
+                string.IsNullOrEmpty(year) ? $"{romajiName}" : $"{romajiName} [{year}]");
+            _logger.LogInformation("FinalDest = {finalDest}", finalDest);
+
+            return (null, finalDest);
+        }
+
+        private IImportFolder GetImportFolder(RelocationEventArgs args)
+        {
+            var dropFolder =
+                args.AvailableFolders.FirstOrDefault(a => a.DropFolderType.HasFlag(DropFolderType.Destination));
+            if (dropFolder != null) return dropFolder;
+
+            _logger.LogError(
+                "No import folders configured as drop source, picking first import folder as destination.");
+            dropFolder = args.AvailableFolders[0];
+
+            return dropFolder;
+        }
+
+        public RelocationResult GetNewPath(RelocationEventArgs args)
+        {
             try
             {
-                dropFolder = args.AvailableFolders.First();
-                try
+                var result = new RelocationResult
                 {
-                    dropFolder = args.AvailableFolders.First(f => f.DropFolderType == DropFolderType.Destination);
-                }
-                catch (InvalidOperationException)
-                {
-                    _logger.LogError(
-                        "No import folders configured as drop source, picking first import folder as destination.");
-                }
-
-                if (args.Series.Count == 0) throw new Exception("Anime not identified");
-                // first determine anime type
-                var anime = args.Series[0];
-                // we use type as string
-                var type = anime.Type.ToString();
-
-                // now we determine anime name to use
-                var romajiName = anime.PreferredTitle.RemoveInvalidPathCharacters();
-
-                // and finally year
-                var year = anime.AirDate.HasValue ? anime.AirDate.Value.Year.ToString() : "";
-
-                finalDest = Path.Combine(type,
-                    string.IsNullOrEmpty(year) ? $"{romajiName}" : $"{romajiName} [{year}]");
-                _logger.LogInformation("FinalDest = {finalDest}", finalDest);
+                    DestinationImportFolder = GetImportFolder(args)
+                };
+                (result.Error, result.FileName) = GetFilename(args);
+                if (result.Error != null)
+                    return result;
+                (result.Error, result.Path) = GetDestination(args);
+                return result;
             }
             catch (Exception e)
             {
-                _logger.LogError(e, "GetDestination error");
-                throw;
+                return new RelocationResult { Error = new RelocationError(e.Message, e) };
             }
-
-            return (dropFolder, finalDest);
         }
     }
 }
